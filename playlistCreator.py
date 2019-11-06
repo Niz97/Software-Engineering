346aa1c8e083b897649f8f9cf06c0532f85a39e0
stiffers <35315214+stiffers@users.noreply.github.com>
2019-11-06 20:25:54 +0000

    API connection to spotify
    Connects to the API spotify using the credentials of a pre created account. It connects with the scope of being able to alter public playlists

---
+++ playlistCreator.py
@@ -0,0 +1,30 @@
+import os
+
+import uuid
+from datetime import date
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
+
+unqiueID = uuid.uuid4()
+today = date.today()
+date = today.strftime("%d/%m/%Y")
+
+cid ="1d2c347f4e3d4b1e916066709832a6fa" 
+secret = "c8a5fa505def47f984bf78021d91b5f5"
+username = "x0pp22txvfpmd630upcjc573a"
+SPOTIPY_REDIRECT_URL = 'http://localhost/'
+
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+scope = 'playlist-modify-public'
+token = util.prompt_for_user_token(username, scope, cid, secret,SPOTIPY_REDIRECT_URL)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+else:
+    print("Can't get token for", username)
+
+
+os.system("pause")
