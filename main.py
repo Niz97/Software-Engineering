666a35d356f1d2c5fb96a955e75434c34014e4a7
stiffers <35315214+stiffers@users.noreply.github.com>
2019-11-07 14:28:46 +0000

    function createPlaylist() defined
    Put the whole file into a function so it can now be called from the main file

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ from newsapi import NewsApiClient
 from monkeylearn import MonkeyLearn
 
 from keywordExtractor import ExtractKeywords
+from playlistCreator import createPlaylist
 
 
 newsApi = NewsApiClient(api_key = '3bd762aea6134796b564d8e18df60cf8') # handle authentication with a News API key (registered using zjcarvin@outlook.com)
 mlApi = MonkeyLearn('e4aabc61a7a365fdf7bda3ab14c7be6c5007d930') # handle authentication with a MonkeyLearn API key (registered using tomzjc@qq.com)
 
-keywordList = ExtractKeywords(newsApi, mlApi, 'gb')
-print(keywordList)
+#keywordList = ExtractKeywords(newsApi, mlApi, 'gb')
+#print(keywordList)
+
+keywordTest = ['mi cc9 pro', 'xiaomi mi cc9', 'isocell bright hmx', 'camera technology', 'camera algorithm', 'handset', 'consumer tech giant', 'community of engineer', 'bright gw1 sensor', 'chinese regulatory agency']
+
+createPlaylist(keywordTest)
--- playlistCreator.py
+++ playlistCreator.py
@@ -6,46 +6,46 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 
-unqiueID = uuid.uuid4()
-today = date.today()
-date = today.strftime("%d/%m/%Y")
-trackIDs = []
+def createPlaylist(keywords):
 
-cid ="1d2c347f4e3d4b1e916066709832a6fa" 
-secret = "c8a5fa505def47f984bf78021d91b5f5"
-username = "x0pp22txvfpmd630upcjc573a"
-SPOTIPY_REDIRECT_URL = 'http://localhost/'
+	unqiueID = uuid.uuid4()
+	today = date.today()
+	newsDate = today.strftime("%d/%m/%Y")
+	trackIDs = []
 
-client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+	cid ="1d2c347f4e3d4b1e916066709832a6fa" 
+	secret = "c8a5fa505def47f984bf78021d91b5f5"
+	username = "x0pp22txvfpmd630upcjc573a"
+	SPOTIPY_REDIRECT_URL = 'http://localhost/'
 
-scope = 'playlist-modify-public'
-token = util.prompt_for_user_token(username, scope, cid, secret,SPOTIPY_REDIRECT_URL)
+	client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
+	sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-else:
-    print("Can't get token for", username)
+	scope = 'playlist-modify-public'
+	token = util.prompt_for_user_token(username, scope, cid, secret,SPOTIPY_REDIRECT_URL)
+
+	if token:
+	    sp = spotipy.Spotify(auth=token)
+	else:
+	    print("Can't get token for", username)
 
 
-playlist_name = date+"-"+str(unqiueID)
-playlists = sp.user_playlist_create(username, playlist_name)
+	playlist_name = newsDate+"-"+str(unqiueID)
+	playlists = sp.user_playlist_create(username, playlist_name)
 
-searchArray = ['mi cc9 pro', 'xiaomi mi cc9', 'isocell bright hmx', 'camera technology', 'camera algorithm', 'handset', 'consumer tech giant', 'community of engineer', 'bright gw1 sensor', 'chinese regulatory agency']
+	for term in keywords:
+		try:
+			result = sp.search("track:"+term, type="track")
+			item = result['tracks']['items']
+			trackIDs.append(item[0]['uri'])
+		except:
+			print("no track found")
 
-for term in searchArray:
-	try:
-		result = sp.search("track:"+term, type="track")
-		item = result['tracks']['items']
-		trackIDs.append(item[0]['uri'])
-	except:
-		print("no track found")
+	playlists = sp.user_playlists(username)
+	for playlist in playlists['items']:
+	    if playlist['owner']['id'] == username:
+	        if playlist['name'] == playlist_name:
+	            for i in range(len(trackIDs)):
+	                sp.user_playlist_add_tracks(username, playlist['id'], trackIDs)
 
-playlists = sp.user_playlists(username)
-for playlist in playlists['items']:
-    if playlist['owner']['id'] == username:
-        if playlist['name'] == playlist_name:
-            for i in range(len(trackIDs)):
-                sp.user_playlist_add_tracks(username, playlist['id'], trackIDs)
-
-os.system("pause")
+	os.system("pause")
