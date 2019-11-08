7f74d1e6b9c5c7e55027bace7c5e21443f6f374b
ArvinZJC <tomzjc@qq.com>
2019-11-08 15:50:01 +0000

    Fix bugs and improve performance
    Keyword extractor (V2.5.0.20191108):
    1. It doesn't use the monkeylearn package any more because the package is not free to use the full functionality. Keyword extraction has to go back to the part of Version 1. Actually, although it provides less accurate results than monkeylearn, it is faster and more stable than others. Further work will be done on improve its performance.
    2. After adding a try...except...else statement and "stripping" each URL, newspaper3k 404 client error and some other connection errors have now been avoided.
    3. Logic has been optimised to improve the extractor's performance.
    
    Closes #1

--- keywordExtractor.py
+++ keywordExtractor.py
@@ -1,80 +1,48 @@
 '''
-@Description: function that extracts keywords from top headlines in the specified country
-@Version: 2.1.0.20191105
-@Author: Jichen Zhao
+@Description: keyword extractor that extracts keywords from the news content downloaded from each URL
+@Version: 2.5.0.20191108
+@Author: Jichen Zhao (driver) and Connor Worthington (observer)
 @Date: 2019-10-29 14:22:59
-@Last Editors: Jichen
-@LastEditTime: 2019-11-05 21:30:25
+@Last Editors: Jichen Zhao
+@LastEditTime: 2019-11-08 15:44:55
 '''
 
 from newspaper import Config, Article
+import nltk
 
 
-def ExtractKeywords(newsApi, mlApi, countryCode):
+def ExtractKeywords(urlList):
     '''
     (TODO:) If the keyword list returned is an empty list, there may be an error or no top headlines got from News API.
     '''
     
-    try:
-        '''
-        get the query result of top headlines in the specified country;
-        for more information about the top headline endpoint, please refer to: https://newsapi.org/docs/endpoints/top-headlines
-        '''
-        topH = newsApi.get_top_headlines(country = countryCode, language = 'en')
-    except Exception as e:
-        #TODO: consider error logs
-        return []
-    else:
-        if (topH['totalResults'] > 0):
-            newsList = topH['articles'] # the value paired with the key "articles" is a list of top headlines got from News API
-            contentList = []
+    if len(urlList) > 0:
+        keywordList = []
 
-            articleConfig = Config()
-            articleConfig.browser_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36' # avoid Newspaper3k 403 Client Error for some URLs
-
-            '''
-            loop to get a list of all top headline content;
-            generally, the value paired with the key "totalResults" is NOT equivalent to the length of the list of top headlines got from News API because not all available top headlines are returned from the server
-            '''
-            for count in range(len(newsList) - 1):
-                news = newsList[count] # each entry of the list is a dictionary of the info of 1 top headline
-
-                content = Article(news['url'], language = 'en', config = articleConfig)
-                try:
-                    content.download()
-                    content.parse()
-                except:
-                    print("cant download")
-                contentList.append(content.text)
+        articleConfig = Config()
+        articleConfig.browser_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36' # avoid Newspaper3k 403 Client Error for some URLs
+        nltk.download('punkt') # TODO: only needs once, this file is required by the following function nlp()
+        
+        # loop to get a list of all keywords
+        for count in range(len(urlList) - 1):
+            content = Article(urlList[count].strip(), language = 'en', config = articleConfig)
+            content.download()
 
             try:
-                '''
-                get the keyword extraction report after analysing all top headline content;
-                the value "ex_YCya9nrn" indicates that the module used is Keyword Extractor;
-                for more information about MonkeyLearn's Extractor API, please refer to: https://monkeylearn.com/api/v3/#extractor-api
-                '''
-                kExtraction = mlApi.extractors.extract('ex_YCya9nrn', contentList)
+                content.parse()
             except Exception as e:
-                #TODO: consider error logs
-                return []
+                # TODO: consider error logs
+                print('URL error')
             else:
-                keywordList = []
+                content.nlp()
+                keywordList.append(content.keywords) # TODO: the order of keywords always changes, too many
 
-                # loop to get a list of all keywords
-                for count in range(len(kExtraction.body) - 1):
-                    kExResult = kExtraction.body[count] # get the keyword extraction result of the specified top headline content
+        # true if Newspaper3k fails to parse the news content downloaded from all URLs
+        if len(keywordList) == 0:
+            # TODO: consider logs
+            print('Keyword extraction failure')
 
-                    if (kExResult['error'] == False):
-                        keywords = []
-
-                        for keywordInfo in kExResult['extractions']:
-                            keywords.append(keywordInfo['parsed_value'])
-
-                        keywordList.append(keywords)
-                    #else: # TODO: what should be printed if some extract keywords successfully...
-                        #print('Error! Failed to extract keywords.\n', kExResult['error_detail'])
-                
-                return keywordList   
-        else:
-            #TODO: consider logs
-            return []
+        return keywordList
+    else:
+        # TODO: consider logs
+        return []
