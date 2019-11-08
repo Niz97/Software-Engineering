8f92aa3bfcdd8223e2cafecc8b2c8010bc2548e2
ArvinZJC <tomzjc@qq.com>
2019-11-08 16:03:40 +0000

    Modified to fit in changes
    1. Remove monkeylearn because it is not free to use the full functionality.
    2. Move authentication handling with newsapi to the pullNews module.
    3. Optimise the workflow of modules.

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 '''
 @Description: main program
-@Version: 1.1.0.20191029
+@Version: 1.5.0.20191109
 @Author: Alexandra Garton, Connor Worthington, Jichen Zhao, Niran Prajapati, and William Staff
 @Date: 2019-10-22 15:22:59
-@Last Editors: Jichen Zhao
-@LastEditTime: 2019-10-29 15:48:27
+@Last Editors: William Staff
+@LastEditTime: 2019-11-08 14:40:09
 '''
-from newsapi import NewsApiClient
-from monkeylearn import MonkeyLearn
+
 from tkinter import *
 
+from pullNews import get_headlines
 from keywordExtractor import ExtractKeywords
 from playlistCreator import createPlaylist
 
 
-newsApi = NewsApiClient(api_key = '3bd762aea6134796b564d8e18df60cf8') # handle authentication with a News API key (registered using zjcarvin@outlook.com)
-mlApi = MonkeyLearn('e4aabc61a7a365fdf7bda3ab14c7be6c5007d930') # handle authentication with a MonkeyLearn API key (registered using tomzjc@qq.com)
-
 def callFuncs():
-	keywordList = ExtractKeywords(newsApi, mlApi, 'gb')
-	createPlaylist(keywordList)
+    urlList = get_headlines('gb')
+    keywordList = ExtractKeywords(urlList)
+    createPlaylist(keywordList)
 
 class Window(Frame):
 
@@ -50,9 +48,4 @@ root = Tk()
 root.geometry("400x300")
 
 app = Window(root)
-root.mainloop()  
-
-#print(keywordList)
-
-#keywordTest = ['mi cc9 pro', 'xiaomi mi cc9', 'isocell bright hmx', 'camera technology', 'camera algorithm', 'handset', 'consumer tech giant', 'community of engineer', 'bright gw1 sensor', 'chinese regulatory agency']
-
+root.mainloop()
