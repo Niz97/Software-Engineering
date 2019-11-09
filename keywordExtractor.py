659f0002cbf28aef8fd0aefc9abbcdf550f24c90
ArvinZJC <zjcarvin@outlook.com>
2019-11-09 06:57:08 +0000

    Fix a bug and start using logging
    1. A bug in looping to get a list of all keywords has been fixed.
    2. Use the module logTool to add a record to the running log accordingly, which benefits both developers and users.
    
    Co-Authored-By: connorworthington99 <connorworthington99@users.noreply.github.com>

--- keywordExtractor.py
+++ keywordExtractor.py
@@ -1,19 +1,24 @@
 '''
-@Description: keyword extractor that extracts keywords from the news content downloaded from each URL
-@Version: 2.5.0.20191108
+@Description: keyword extractor that can extract keywords from the news content downloaded from each URL
+@Version: 2.7.2.20191109
 @Author: Jichen Zhao (driver) and Connor Worthington (observer)
 @Date: 2019-10-29 14:22:59
 @Last Editors: Jichen Zhao
-@LastEditTime: 2019-11-08 15:44:55
+@LastEditTime: 2019-11-09 06:50:02
 '''
 
 from newspaper import Config, Article
 import nltk
 
+from logTool import Log
 
-def ExtractKeywords(urlList):
+
+def ExtractKeywords(urlList: list) -> list:
     '''
-    (TODO:) If the keyword list returned is an empty list, there may be an error or no top headlines got from News API.
+    This function can extract keywords from the news content downloaded from each URL.
+
+    :param urlList: a list of URLs containing the news content for keyword extraction
+    :returns: a list of all keywords extracted
     '''
     
     if len(urlList) > 0:
@@ -21,28 +26,37 @@ def ExtractKeywords(urlList):
 
         articleConfig = Config()
         articleConfig.browser_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36' # avoid Newspaper3k 403 Client Error for some URLs
-        nltk.download('punkt') # TODO: only needs once, this file is required by the following function nlp()
+        nltk.download('punkt') # the package punkt is required by the following function nlp()
         
         # loop to get a list of all keywords
-        for count in range(len(urlList) - 1):
+        for count in range(len(urlList)):
             content = Article(urlList[count].strip(), language = 'en', config = articleConfig)
             content.download()
 
             try:
                 content.parse()
             except Exception as e:
-                # TODO: consider error logs
-                print('URL error')
+                Log('error', repr(e))
             else:
                 content.nlp()
                 keywordList.append(content.keywords) # TODO: the order of keywords always changes, too many
 
-        # true if Newspaper3k fails to parse the news content downloaded from all URLs
+        # true if Newspaper3k fails to parse any news content downloaded
         if len(keywordList) == 0:
-            # TODO: consider logs
-            print('Keyword extraction failure')
+            Log('warning', 'It seems that Newspaper3k fails to parse any news content downloaded.')
 
         return keywordList
     else:
-        # TODO: consider logs
+        Log('warning', 'The URL list is empty.')
         return []
+
+
+# test purposes only
+if __name__ == '__main__':
+    from pullNews import get_headlines
+
+    urlList = get_headlines('gb')
+    keywordList = ExtractKeywords(urlList)
+    
+    print(len(urlList), 'URL(s):\n', urlList)
+    print(len(keywordList), 'group(s) of keywords:\n', keywordList)
