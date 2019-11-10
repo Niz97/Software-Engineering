d3a95dca6fdcf8b1f030268aaa386bd7f46493ce
ArvinZJC <zjcarvin@outlook.com>
2019-11-10 04:30:39 +0000

    Fix a bug and optimise performance
    1. The name of the function calling the function Log can be displayed correctly now.
    2. Simplify the logging format.
    3. Remove support for the level CRITICAL.
    
    Co-Authored-By: connorworthington99 <connorworthington99@users.noreply.github.com>

--- logTool.py
+++ logTool.py
@@ -1,27 +1,33 @@
 '''
 @Description: log tool that can take a log of messages with the level INFO, WARNING, ERROR, or CRITICAL
-@Version: 1.0.0.20191109
+@Version: 1.2.0.20191110
 @Author: Jichen Zhao (driver) and Connor Worthington (observer)
 @Date: 2019-11-09 06:00:48
 @Last Editors: Jichen Zhao
-@LastEditTime: 2019-11-09 06:44:03
+@LastEditTime: 2019-11-10 04:27:04
 '''
 
+import inspect
 import logging
 
 
-def Log(level: 'str', message) -> None:
+def Log(level: 'str', message: 'str') -> None:
     '''
-    This function can add a message with the level INFO, WARNING, ERROR, or CRITICAL to the specified running log.
+    This function can add a message with the level INFO, WARNING, or ERROR to the specified running log.
 
-    :param level: the level of a message (accepted values: 'info', 'warning', 'error', and 'critical')
+    :param level: the level of a message (accepted values: 'info', 'warning', and 'error')
     :param message: the message decided to show in the related record of the running log
     :raises Exception: a common exception will be raised if the level of a message is invalid
     '''
 
+    message = inspect.stack()[1][3] + ': ' + message # add the name of the function calling this function to the message
+
     logging.basicConfig(
-        level = logging.INFO, filename = 'appName.log', filemode = 'a',
-        format = '%(asctime)s - %(module)s\%(funcName)s[line:%(lineno)d] - %(levelname)s: %(message)s') # TODO: filename
+        level = logging.INFO,
+        filename = 'appName.log', # TODO: filename
+        filemode = 'a',
+        datefmt="%Y-%m-%d %H:%M:%S",
+        format = '%(asctime)s - %(levelname)s - %(message)s')
 
     if level == 'info':
         logging.info(message)
@@ -29,20 +35,16 @@ def Log(level: 'str', message) -> None:
         logging.warning(message)
     elif level == 'error':
         logging.error(message)
-    elif level == 'critical':
-        logging.critical(message)
     else:
         raise Exception('Invalid level of a message.')
 
 
 # test purposes only
 if __name__ == '__main__':
-    Log('info', True)
-    Log('warning', 404)
+    Log('info', 'Hello World!')
+    Log('warning', 'Nothing in the list.')
 
     try:
         2 / 0
     except Exception as e:
-        Log('error', e)
-    
-    Log('critical', 'No such module.')
+        Log('error', repr(e))
