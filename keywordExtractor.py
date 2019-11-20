38f41772cc5bbccec83de03f6a22fa36135c52be
ArvinZJC <zjcarvin@outlook.com>
2019-11-20 15:35:22 +0000

    Improve performance
    1. Implement a function for each multithreading which parses news content and extracts keywords.
    2.
    Make the keyword extractor a class and move the list keywordList to a constructor.
    
    Co-Authored-By: ally-garton <ally-garton@users.noreply.github.com>

--- keywordExtractor.py
+++ keywordExtractor.py
@@ -1,11 +1,13 @@
 '''
 @Description: keyword extractor that can extract keywords from the news content downloaded from each URL
-@Version: 3.3.0.20191110
-@Author: Jichen Zhao (driver) and Connor Worthington (observer)
+@Version: 3.7.0.20191118
+@Author: Jichen Zhao (driver) and Alexandra Garton (observer)
 @Date: 2019-10-29 14:22:59
 @Last Editors: Jichen Zhao
-@LastEditTime: 2019-11-10 21:12:41
+@LastEditTime: 2019-11-20 15:34:25
 '''
+
+import threading
 
 from newspaper import Config, Article
 from gensim.summarization import keywords
@@ -13,24 +15,30 @@ from gensim.summarization import keywords
 from logTool import Log
 
 
-def ExtractKeywords(urlList: list) -> list:
-    '''
-    This function can extract keywords from the news content downloaded from each URL.
+class KeywordExtractor():
+    def __init__(self):
+        self.keywordList = []
+        self.articleConfig = Config()
+        self.articleConfig.browser_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36' # avoid Newspaper3k 403 Client Error for some URLs
 
-    :param urlList: a list of URLs from which the news content will be downloaded for keyword extraction
-    :returns: a keyword list whose elements are lists of keywords extracted from each news (generally no more than 5 words in each list)
-    '''
-    
-    if len(urlList) > 0:
-        keywordList = []
 
-        articleConfig = Config()
-        articleConfig.browser_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36' # avoid Newspaper3k 403 Client Error for some URLs
-        
-        # loop to get each URL and to extract keywords from the news content downloaded
-        for count in range(len(urlList)):
-            url = urlList[count].strip()
-            content = Article(url, language = 'en', config = articleConfig)
+    def ExtractKeywords(self, urlList: list) -> list:
+        '''
+        Extract keywords from the news content downloaded from each URL.
+
+        :param urlList: a list of URLs from which the news content will be downloaded for keyword extraction
+        :returns: a keyword list whose elements are lists of keywords extracted from each news (generally no more than 5 words in each list)
+        '''
+
+
+        def ThreadTask(self, url: str):
+            '''
+            The thread allocated should extract keywords from the news content downloaded from a given URL.
+
+            :param url: a given URL from which the news content will be downloaded for keyword extraction
+            '''
+
+            content = Article(url, language = 'en', config = self.articleConfig)
             content.download()
 
             try:
@@ -54,27 +62,45 @@ def ExtractKeywords(urlList: list) -> list:
                         content.text,
                         lemmatize = True, # lemmatise words (e.g. ['dances'] instead of ['dancing', 'dance', 'dances'])
                         split = True)
-                
+                        
                 if len(keywordList_element) > 0:
-                    keywordList.append(keywordList_element)
+                    self.keywordList.append(keywordList_element)
                 else:
                     Log('warning', 'The list of keywords extracted from a piece of news is empty. Related URL: ' + url)
+        
 
-        if len(keywordList) == 0:
-            Log('warning', 'The keyword list is empty.')
+        if len(urlList) > 0:
+            threadList = []
+            
+            # loop to start a thread for each URL to try to extract keywords quickly
+            for count in range(len(urlList)): # range: [0, len(urlList))
+                url = urlList[count].strip()
 
-        return keywordList
-    else:
-        Log('warning', 'The URL list is empty.')
-        return []
+                newThread = threading.Thread(target = ThreadTask, args = (self, url,)) # extract keywords from the news content downloaded from a URL
+                threadList.append(newThread)
+                newThread.start()
+            
+            for thread in threadList:
+                thread.join()
+
+            if len(self.keywordList) == 0:
+                Log('warning', 'The keyword list is empty.')
+        else:
+            Log('warning', 'The URL list is empty.')
+        
+        return self.keywordList
 
 
 # test purposes only
 if __name__ == '__main__':
     from pullNews import get_headlines
 
-    urlList = get_headlines('gb')
-    print(len(urlList), 'URL(s):\n', urlList)
+    for source in ['bbc-news', 'the-wall-street-journal', 'google-news']:
+        print(source)
 
-    keywordList = ExtractKeywords(urlList)
-    print(len(keywordList), 'list(s) of keywords:\n', keywordList)
+        urlList = get_headlines('bbc-news')[1]
+        print(len(urlList), 'URL(s):\n', urlList)
+
+        extractor = KeywordExtractor()
+        keywordList = extractor.ExtractKeywords(urlList)
+        print(len(keywordList), 'list(s) of keywords:\n', keywordList)
