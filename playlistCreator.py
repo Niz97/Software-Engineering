434b54c07674839fb7e40b87683e8e6e9e0f8216
ArvinZJC <zjcarvin@outlook.com>
2019-11-20 15:31:33 +0000

    Optimise the playlist
    1. Name a playlist in a format of “news source-time-date”.
    2.
    The logic of adding tracks to a playlist is optimised so that it can now make the number of tracks in a playlist much more reasonable.
    3.
    Add a logic of generating a playlist properly when the number of tracks prepared to be added to a playlist is 0 or over 100.
    
    Co-Authored-By: stiffers <stiffers@users.noreply.github.com>

--- playlistCreator.py
+++ playlistCreator.py
@@ -1,63 +1,51 @@
+'''
+@Description: Create a Spotify playlist.
+@Version: 2.2.0.20191118
+@Author: William Staff (driver) and Jichen Zhao(observer)
+@Date: 2019-11-06 23:55:01
+@Last Editors: Jichen Zhao
+@LastEditTime: 2019-11-20 15:30:02
+'''
+
 import os
 
-import uuid
-from datetime import date
+from datetime import datetime
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 from logTool import Log
 
-def createPlaylist(keywords):
+def createPlaylist(token, newsSource, keywords):
 
-	unqiueID = uuid.uuid4()
-	today = date.today()
-	newsDate = today.strftime("%d/%m/%Y")
+	time = datetime.today()
+	newsDate = time.strftime('%H:%M-%d/%m/%Y')
 	trackIDs = []
 
-	cid ="b80c9d43d197499f8df5e1e438331a3a" 
-	secret = "82ed92de9f1a450a85115d342ca696e2"
-	username = "uu8bhrprx5sfo9oasyf36hbxz"
-	SPOTIPY_REDIRECT_URL = 'http://localhost/'
-
-	client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
-	sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-	scope = 'playlist-modify-public'
-	token = util.prompt_for_user_token(username, scope, cid, secret,SPOTIPY_REDIRECT_URL)
-
 	if token:
-	    sp = spotipy.Spotify(auth=token)
-	else:
-	    Log('warning', "Can't get token for" + username)
-
-
-	playlist_name = newsDate+"-"+str(unqiueID)
-	playlists = sp.user_playlist_create(username, playlist_name)
-
-
-	for termArray in keywords:
-		for term in termArray:
-			try:
-				result = sp.search("track:"+term, type="track")
+		sp = spotipy.Spotify(auth = token)
+		username = sp.current_user()['id']
+		playlist_name = newsSource + '-' + newsDate
+		playlists = sp.user_playlist_create(username, playlist_name)
+		
+		for termArray in keywords:
+			for term in termArray:
+				result = sp.search('track:'+term, type='track', limit = 1)
 				item = result['tracks']['items']
-				trackIDs.append(item[0]['uri'])
-			except Exception as e:
-				Log('error', repr(e))
-
-
-	playlists = sp.user_playlists(username)
-	for playlist in playlists['items']:
-	    if playlist['owner']['id'] == username:
-	        if playlist['name'] == playlist_name:
-	            for i in range(len(trackIDs)):
-	                sp.user_playlist_add_tracks(username, playlist['id'], trackIDs)
-
-
-# test purposes only
-if __name__ == '__main__':
-    from pullNews import get_headlines
-    from keywordExtractor import ExtractKeywords
-
-    urlList = get_headlines('gb')
-    keywordList = ExtractKeywords(urlList)
-    createPlaylist(keywordList) # TODO: what if the keyword list is empty?
+				
+				if len(item) > 0:
+					trackIDs.append(item[0]['uri'])
+		for i in range(0,70):
+			trackIDs.append(trackIDs[0])
+		if len(trackIDs) == 0:
+			Log('warning', 'The playlist is empty.')
+		else:
+			if len(trackIDs) > 100:
+				sp.user_playlist_add_tracks(username, playlists['id'], [trackIDs[count] for count in range(100)])
+				sp.user_playlist_add_tracks(username, playlists['id'], [trackIDs[count] for count in range(100, len(trackIDs))])
+			else:
+				sp.user_playlist_add_tracks(username, playlists['id'], trackIDs)
+		
+		return playlists['external_urls']['spotify']
+	else:
+		Log('warning', 'Cannot get Spotify access token.')
+		return ''
